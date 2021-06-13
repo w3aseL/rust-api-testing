81e915f1967a5b6288b71a09bfc175e45dd2fb6d
Noah Templet <noahtemp@atvci.net>
2021-06-13 14:23:26 -0500

    Image loading works now. Can be improved upon.

--- src/api/routes/images.rs
+++ src/api/routes/images.rs
@@ -1,12 +1,11 @@
 use rocket::{ Data };
 use rocket::http::{ ContentType, Status };
 use image::io::{ Reader as ImageReader };
-use std::io::{self, Cursor, Write};
+use image::imageops;
+use std::fmt::Error;
+use std::io::{self, Cursor, Read, Write};
 
-use multipart::mock::StdoutTee;
 use multipart::server::Multipart;
-use multipart::server::save::Entries;
-use multipart::server::save::SaveResult::*;
 
 use crate::api::server::{ JsonResponse, SimpleResponse, ErrorResponse };
 
@@ -18,32 +17,20 @@ fn process_upload(boundary: &str, data: Data) -> io::Result<Vec<u8>> {
     // saves all fields, any field longer than 10kB goes to a temporary directory
     // Entries could implement FromData though that would give zero control over
     // how the files are saved; Multipart would be a good impl candidate though
-    match Multipart::with_body(data.open(), boundary).save().temp() {
-        Full(entries) => process_entries(entries, &mut out)?,
-        Partial(partial, reason) => {
-            writeln!(out, "Request partially processed: {:?}", reason)?;
-            if let Some(field) = partial.partial {
-                writeln!(out, "Stopped on field: {:?}", field.source.headers)?;
-            }
+    let mut mp = Multipart::with_body(data.open(), boundary);
+	mp.foreach_entry(|mut entry| match &*entry.headers.name {
+		"image" => {
+			info!("Received file with name {}", entry.headers.filename.unwrap());
 
-            process_entries(partial.entries, &mut out)?
-        },
-        Error(e) => return Err(e),
-    }
+			entry.data.read_to_end(&mut out).expect("No file found!");
+		},
+		_ => {
+			info!("Found an unknown entry...")
+		}
+	})
+	.expect("Failed to iterate through entries!");
 
     Ok(out)
-}
-
-// having a streaming output would be nice; there's one for returning a `Read` impl
-// but not one that you can `write()` to
-fn process_entries(entries: Entries, mut out: &mut Vec<u8>) -> io::Result<()> {
-    {
-        let stdout = io::stdout();
-        let tee = StdoutTee::new(&mut out, &stdout);
-        entries.write_debug(tee)?;
-    }
-
-    writeln!(out, "Entries processed")
 }
 
 #[post("/upload-image", data="<data>")]
@@ -62,14 +49,18 @@ pub fn upload_image(content_type: &ContentType, data: Data) -> Result<JsonRespon
 
 	match process_upload(boundary, data) {
         Ok(resp) => {
-			let img = match ImageReader::new(Cursor::new(resp)).decode() {
+			let mut img = match ImageReader::new(Cursor::new(resp)).with_guessed_format().expect("Failed to guess format!").decode() {
 				Ok(img) => img,
-				Err(_) => return Err(JsonResponse::parse_new(Status::InternalServerError, SimpleResponse {
-					message: "Failed to read image data sent!"
-				}))
+				Err(err) => {
+					return Err(JsonResponse::parse_new(Status::InternalServerError, ErrorResponse {
+						error: err.to_string()
+					}))
+				}
 			};
-			
-			if let Err(_) = img.save("test.jpg") {
+
+			img.invert();
+
+			if let Err(_) = img.save("test_blur.jpg") {
 				return Err(JsonResponse::parse_new(Status::InternalServerError, SimpleResponse {
 					message: "Failed to save the file!"
 				}))
